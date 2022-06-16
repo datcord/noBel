ff1ca73e53b74485029d1646b0621aa4a31ea45a
Theodore Zisimopoulos <57064762+datcord@users.noreply.github.com>
2022-06-16 12:58:34 +0300

    custom toolbar testing

--- app/src/main/java/com/example/nobel/MainActivity.java
+++ app/src/main/java/com/example/nobel/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.nobel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
